1d7f44112c891e64fc37c49d98916e00dee891e0
Anthony Phillips <github.com@tonybphillips.com>
2012-04-04 17:18:03 -0700

    adds devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChangeOrg::Application.config.secret_token = 'e46e833489380ab88de03a040f15bf58b6b3c854af9c3608b76fae63271dbcf60f6952042cb2d0989c0103944a941aeb19d01d4b1641129dea72af020dd54e7a'
